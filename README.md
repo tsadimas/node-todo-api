921159fbed8d947aa52fb58e394b3c77d7aee40c
tsadimas <tsadimas@hua.gr>
2018-03-06 17:54:28 +0200

    added mlad database

---
+++ README.md
@@ -0,0 +1,10 @@
+Create a keys.json file in project root, adding
+
+```javascript
+{
+  "mongoHost": "YOUR_MONGO_HOST",
+  "mongoPort": "YOUR_MONGO_PORT",
+  "mongoDatabase": "YOUR_MONGO_DB",
+  "mongoUser": "YOUR_MONGO_USERNAME",
+  "mongoPass": "YOUR_MONGO_PASSWORD"
+}
--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,38 +1,57 @@
+const nconf = require('nconf');
+const fs = require('fs');
+
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID}= require('mongodb');
 
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,client) => {
-    if (err) {
-        return console.lor('Unable to connect to MongoDB server');
-    }
+nconf.argv().env().file({ file: 'keys.json'});
 
-    console.log('Connected to MongoDB server');
-    const db = client.db('TodoApp');
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
 
-    //     db.collection('Todos').insertOne({
-            //             text: 'Something to do',
-            //             completed: false
-            //     }, (err, result) => {
-                //         if (err) {
-                    //             return console.log('Unable to insert todo', err);
-                    //         }
-                //         console.log(JSON.stringify(result.ops, undefined, 2));
-                //     })
+console.log(user);
+var uri = `mongodb://${user}:${pass}@${host}:${port}`;
+if (nconf.get('mongoDatabase')) {
+    uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+console.log(uri);
+
+
+//MongoClient.connect('mongodb://argiris:***REMOVED***@***REMOVED***/?authMechanism=SCRAM-SHA-1&authSource=***REMOVED***', (err,client) => {
+
+    MongoClient.connect(uri, (err, client) => {
+        if (err) {
+            return console.log(`Unable to connect to MongoDB server ${err}`);
+        }
+
+        console.log('Connected to MongoDB server');
+        const db = client.db('***REMOVED***');
+        db.collection('Todos').insertOne({
+                text: 'Something to do',
+                completed: false
+        }, (err, result) => {
+            if (err) {
+                return console.log('Unable to insert todo', err);
+            }
+            console.log(JSON.stringify(result.ops, undefined, 2));
+        });
 
         // insert new doc into  the Users (name, age, location)
-      //  db.collection('Users').insertOne({
-      //          name: 'Argiris',
-      //          age: 38,
-      //          location: 'Athens'
-      //  }, (err, result) => {
-      //      if (err) {
+        //  db.collection('Users').insertOne({
+                //          name: 'Argiris',
+                //          age: 38,
+                //          location: 'Athens'
+                //  }, (err, result) => {
+                    //      if (err) {
 
-      //          return console.log('Unable to insert user', err);
-      //      }
+                        //          return console.log('Unable to insert user', err);
+                        //      }
 
-      //      console.log(result.ops[0]._id.getTimestamp());
-      //  });
+                    //      console.log(result.ops[0]._id.getTimestamp());
+                    //  });
 
-        client.close();
-});
+            client.close();
+    });
