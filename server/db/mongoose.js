c345853060d11eb826c6f335ba7d13422d2ab998
Anargyros Tsadimas <tsadimas@gmail.com>
2018-03-08 20:36:37 +0200

    Add POST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+const nconf = require('nconf');
+const fs = require('fs');
+
+nconf.argv().env().file({ file: 'keys.json'});
+
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
+
+console.log(user);
+var uri = `mongodb://${user}:${pass}@${host}:${port}`;
+if (nconf.get('mongoDatabase')) {
+    uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+console.log(uri);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+module.exports = {mongoose};
